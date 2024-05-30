985f9ea101671cc0f9e81fb3e0d3845ed352c723
Roberto0611 <ochoacuevasr@gmail.com>
2024-05-30 15:13:55 -0700

    Finished

---
+++ script.js
@@ -0,0 +1,7 @@
+function showLetter() {
+    document.getElementById('letter').classList.toggle('hidden');
+}
+
+function showSecret() {
+    document.getElementById('secret-message').classList.toggle('hidden');
+}
